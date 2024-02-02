d82c715d5066deec0c9cd21e2a3aafa1eb275f45
Dalton Tewanger <daltontewanger@yahoo.com>
2024-02-01 19:39:28 -0800

    Created a map using leaflet and added a marker

--- public/app.js
+++ public/app.js
@@ -1,20 +1,20 @@
-//Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
-
-
-
-// Create map
-
-
+// Create map                                                     
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
 
 
 // Add openstreetmap tiles
-
-
-
-//Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
 
 // Create and add a geolocation marker
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 
